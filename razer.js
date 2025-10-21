ec6c5a7814e3364fc63a1234d77ec0537d4a8e91
pulkit334 <gpulkit744@gmail.com>
2025-10-21 15:00:57 +0530

    add razer.js

---
+++ razer.js
@@ -0,0 +1,10 @@
+// config/razer.js
+const Razorpay = require("razorpay");
+require("dotenv").config();
+
+const razorpay = new Razorpay({
+  key_id: process.env.key_id,
+  key_secret: process.env.key_secret,
+});
+
+module.exports = razorpay;
